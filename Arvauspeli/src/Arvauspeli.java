4e910d1e473092f06e00a6c278c5566a5e4cd844
Aaro55 <aaro.puranen@gmail.com>
2025-10-22 10:11:02 +0300

    Setup

---
+++ Arvauspeli/src/Arvauspeli.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Arvauspeli {
+    public static void main(String[] args) throws Exception {
+    Scanner scanner = new Scanner(System.in);
+
+    String salaisuus = "Emma";
+    String muuttuja;
+    int yritys = 0;
+
+    }
+}
